9d6b6e4623c947ac8a10881496b66b1ca95253fb
edwinaikins <edwinaikins@gmail.com>
2024-10-21 09:48:56 +0000

    Database connection implemented

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+require('dotenv').config();
+const Blog = require("./models/blogs");
+const mongoose = require("mongoose")
+
+const databaseConnect = async () =>{
+    try{
+        await mongoose.connect(process.env.uri)
+        console.log("database connected succesfully")
+        app.listen(port,()=>{
+            console.log(`Server started listening on port:${port}`)
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+
+databaseConnect();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = 3002;
-const blogs = [];
 
 app.use((req,res,next)=>{
     console.log(req.method);
@@ -12,60 +31,77 @@ app.use((req,res,next)=>{
 })
 
 app.post("/posts",(req,res)=>{
-    const {title, content, author} = req.body;
-    let post = {
-        id: blogs.length + 1,
-        title,
-        content,
-        author,
-    }
-    blogs.push(post)
-    res.send("Post sucessfully created")
+    const blog = new Blog(req.body);
+    blog.save()
+    .then(() => res.send("Post successfully created"))
+    .catch(error => {
+            console.log(error);
+            res.status(500).send("Error creating post");
+        });
 })
 
 app.get("/posts", (req,res)=>{
-    if(blogs.length > 0){
-        res.json(blogs)
-    }else{
-        res.json([])
-    }
+    Blog.find()
+        .then((result)=>{
+            res.json(result)
+        })
+        .catch((error) =>{
+            console.log(error)
+            res.status(404).send("blog not found")
+        })
 })
 
 app.get("/posts/:id",(req,res)=>{
-    const id = parseInt(req.params.id);
-    const blog = blogs.find((blog)=> blog.id === id)
-    if(blog){
-        res.json(blog)
-    }else{
-        res.status(404).send("blog not found")
-    }
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result)=>{
+            if(result){
+                res.json(result)
+            }
+            else{
+                res.status(404).send("no blog found")
+            }
+            
+        })
+        .catch(()=>{
+            console.log(error)
+            res.status(500).send("Error Occured")
+        })
 })
 
 app.put("/posts/:id", (req,res)=>{
-    const id = parseInt(req.params.id);
-    const blog = blogs.find((blog)=> blog.id === id)
-    const {title,content,author} = req.body;
-    if(blog){
-        blog.title = title || blog.title;
-        blog.content = content || blog.content;
-        blog.author = author || blog.author;
-        res.json(blog);
-    }else{
+    const id = req.params.id;
+    Blog.findByIdAndUpdate(id, req.body, {new: true})
+        .then((result) =>{
+            if(result){
+                res.json(result)
+            }
+            else{
+                res.status(404).send("Blog not found")
+            }
+        })
+        .catch((error) =>{
+            console.log(error)
+            res.status(500).send("Error Occured")
+        })
         res.status(404).send("no blog to edit");
-    }
 })
 
 app.delete("/posts/:id", (req,res)=>{
-    const id = parseInt(req.params.id);
-    const blogIndex = blogs.findIndex((blog)=> blog.id === id)
-    if(blogIndex !== -1){
-        blogs.splice(blogIndex,1);
-        res.json(blogs)
-    }else{
-        res.status(404).send("blog does not exist")
-    }
-})
-
-app.listen(port,()=>{
-    console.log(`Server started listening on port:${port}`)
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result)=>{
+            if(result) {
+                res.send("Blog deleted")
+            }
+            else{
+                res.status(404).send("Blog not found")
+            }    
+        })
+        .catch((error) =>{
+            console.log(error)
+            res.status(500).send("Error occured")
+        })
+        
+    
 })
